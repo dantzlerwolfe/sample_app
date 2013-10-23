16f10e5b2c8e052c630d8f41b254f10f3798e29d
dantzlerwolfe <dantzlerwolfe@gmail.com>
2013-10-23 08:48:32 -0400

    added postgres

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let (:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "should have the h1 'Sample App'" do
     	visit '/static_pages/home'
@@ -11,7 +14,7 @@ describe "Static pages" do
     it "should have the base title" do
     	visit '/static_pages/home'
     	page.should have_selector('title', 
-    		:text => 'Ruby on Rails Tutorial Sample App')
+    		:text => "#{base_title}")
 
     end
 
@@ -34,7 +37,7 @@ describe "Static pages" do
     it "should have the right title" do
     	visit '/static_pages/help'
     	page.should have_selector('title', 
-    		:text => 'Ruby on Rails Tutorial Sample App | Help')
+    		:text => "#{base_title} | Help")
 
     end
   end
@@ -49,7 +52,7 @@ describe "Static pages" do
     it "should have the right title" do
     	visit '/static_pages/about'
     	page.should have_selector('title', 
-    		:text => 'Ruby on Rails Tutorial Sample App | About Us')
+    		:text => "#{base_title} | About Us")
 
     end
   end
@@ -64,7 +67,7 @@ describe "Static pages" do
     it "should have the right title" do
     	visit '/static_pages/contact'
     	page.should have_selector('title', 
-    		:text => 'Ruby on Rails Tutorial Sample App | Contact')
+    		:text => "#{base_title} | Contact")
 
     end
   end
